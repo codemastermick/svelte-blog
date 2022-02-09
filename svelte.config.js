9a926579262b423b1ad48357d6d70cc2d5ad6fb7
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-09 16:00:58 -0500

    feat: create simple layout for posts

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess'
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: {
+				blog: 'src/routes/blog/_post.svelte'
+			}
+		})
+	],
 
 	kit: {
 		adapter: adapter()
