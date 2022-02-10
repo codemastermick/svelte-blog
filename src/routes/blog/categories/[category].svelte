c6c764ed4eec6efb4fff164fc8ef03eaaf9ce86b
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-10 00:54:44 -0500

    feat: introduce dynamic categories

---
+++ src/routes/blog/categories/[category].svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	export const load = async ({ params, fetch }) => {
+		const currentCategory = params.category;
+		const response = await fetch('/api/posts.json');
+		const posts = await response.json();
+
+		const matchingPosts = posts.filter((post) => post.meta.categories.includes(currentCategory));
+
+		return {
+			props: {
+				posts: matchingPosts
+			}
+		};
+	};
+</script>
+
+<script>
+	import { page } from '$app/stores';
+	export let posts;
+</script>
+
+{#if posts.length}
+	<ul>
+		{#each posts as post}
+			<li>
+				<h2>
+					<a href={post.path}>
+						{post.meta.title}
+					</a>
+				</h2>
+				Published {post.meta.date}
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No posts published under <b>{$page.params.category}</b></p>
+{/if}
