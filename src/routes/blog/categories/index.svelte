0fba44e007608abb7de6504181c4894af61cae65
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-10 09:28:47 -0500

    feat: create list of all categories

---
+++ src/routes/blog/categories/index.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+	export const load = async ({ params, fetch }) => {
+		const response = await fetch('/api/posts.json');
+		const posts = await response.json();
+		let found = [];
+		posts.forEach((p) => {
+			found = [...found, ...p.meta.categories];
+		});
+		const allCategories = [...new Set(found)];
+
+		return {
+			props: {
+				allCategories: allCategories
+			}
+		};
+	};
+</script>
+
+<script>
+	export let allCategories;
+</script>
+
+{#if allCategories.length}
+	<ul>
+		{#each allCategories as category}
+			<li>
+				<a href="/blog/categories/{category}">
+					{category}
+				</a>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No categories found!</p>
+{/if}
