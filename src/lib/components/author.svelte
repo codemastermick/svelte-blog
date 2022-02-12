fcf7db0012abdabc8fa910a14b896b3e60f7a62f
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-12 00:44:07 -0500

    feat: add fallback avatar if none supplied

---
+++ src/lib/components/author.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let name: string;
+  export let avatar: string;
+  if (name === undefined) {
+    name = 'posted by admin';
+  }
+  if (avatar === undefined) {
+    avatar = '../static/generic_avatar.png';
+  }
+</script>
+
+<span>
+  {name}
+  <img src={avatar} alt="Avatar for {name}" />
+</span>
+
+<style lang="scss">
+  img {
+    width: 1.5em;
+  }
+</style>
