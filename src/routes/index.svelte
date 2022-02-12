42af90f0d381f5f2e1bf1bb75ed98751efff2134
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-11 21:01:41 -0500

    chore: show generic text on landing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
+<!-- TODO change this to reference your own info instead -->
 <svelte:head>
   <title>Codemaster's Domain</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Codemaster's Domain</h1>
+<p>
+  This site is based on a personal website/blog by Codemaster Mick. You can fill in this area to
+  display some cool stuff about yourself if you like, and maybe direct the viewer to look around at
+  the links provided.
+</p>
