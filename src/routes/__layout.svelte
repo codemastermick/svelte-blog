3d907feb7b6f9e186f7a30dbb9e12edf8f5ab234
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-12 11:40:34 -0500

    style: adjust transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -15,14 +15,14 @@
   import Header from '$lib/components/header.svelte';
   import Footer from '$lib/components/footer.svelte';
   import '$lib/styles/style.scss';
-  import { fade } from 'svelte/transition';
+  import { fade, fly } from 'svelte/transition';
   export let currentRoute;
   export let owner: string;
 </script>
 
 <Header />
 {#key currentRoute}
-  <main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+  <main in:fly={{ duration: 150, delay: 150, x: 500 }} out:fade={{ duration: 150 }}>
     <slot {owner} />
   </main>
 {/key}
