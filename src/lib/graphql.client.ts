c721a99a8f5ad45ac45df97972769c16338dfba9
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-12 23:58:57 -0500

    feat: add exported functions to graph client

--- src/lib/graphql.client.ts
+++ src/lib/graphql.client.ts
@@ -1,5 +1,58 @@
 import { GraphQLClient } from 'graphql-request';
+import { gql, request } from 'graphql-request';
 
 export const client = new GraphQLClient(import.meta.env.VITE_GRAPHCMS_ENDPOINT as string, {
   headers: {}
 });
+
+export const getBlogRoll = async (): Promise<Partial<PostMetadata>> => {
+  try {
+    const res = await client.request(gql`
+      {
+        articles {
+          title
+          publishDate
+          slug
+        }
+      }
+    `);
+    return res.articles;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getArticle = async (slug: string): Promise<PostMetadata> => {
+  const res = await request(
+    import.meta.env.VITE_GRAPHCMS_ENDPOINT as string,
+    gql`
+      query FetchFull($slug: String!) {
+        article(where: { slug: $slug }) {
+          id
+          title
+          publishDate
+          slug
+          updatedAt
+          author {
+            name
+            email
+            avatar
+          }
+          categories
+          excerpt
+          body
+        }
+      }
+    `,
+    { slug }
+  );
+  return res.article;
+};
+
+export const FetchCategories = gql`
+  {
+    articles {
+      categories
+    }
+  }
+`;
