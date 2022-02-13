4085898f780bfc0ea037575bf62943e6813b41cb
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-13 17:57:29 -0500

    refactor: remove use of $page

--- src/routes/blog/categories/[category].svelte
+++ src/routes/blog/categories/[category].svelte
@@ -1,4 +1,5 @@
 <script context="module">
+  import { getArticlesByCategory } from '$lib/graphql.client';
   export const load = async ({ params, fetch }) => {
     // const currentCategory = params.category;
     // const response = await fetch('/api/posts.json');
@@ -8,6 +9,7 @@
 
     return {
       props: {
+        category: params.category,
         posts: await getArticlesByCategory(params.category)
       }
     };
@@ -15,18 +17,17 @@
 </script>
 
 <script lang="ts">
-  import { page } from '$app/stores';
   import BlogRoll from '$lib/components/blogRoll.svelte';
-  import { getArticlesByCategory } from '$lib/graphql.client';
+  export let category;
   export let posts;
 </script>
 
 <svelte:head>
-  <title>{import.meta.env.VITE_SITENAME} - Posted under "{$page.params.category}"</title>
+  <title>{import.meta.env.VITE_SITENAME} - Posted under "{category}"</title>
 </svelte:head>
 
 {#if posts.length}
   <BlogRoll {posts} />
 {:else}
-  <p>No posts published under <b>{$page.params.category}</b></p>
+  <p>No posts published under <b>{category}</b></p>
 {/if}
