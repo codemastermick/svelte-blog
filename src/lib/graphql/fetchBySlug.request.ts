6860fca477eaa6b7250f257c8f9f62dd5130df2b
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-13 23:18:59 -0500

    fix: remove extraneous ordering

--- src/lib/graphql/fetchBySlug.request.ts
+++ src/lib/graphql/fetchBySlug.request.ts
@@ -2,7 +2,7 @@ import { gql } from 'graphql-request';
 
 export const fetchBySlug = gql`
   query FetchFull($slug: String!) {
-    article(where: { slug: $slug }, orderBy: publishedAt_DESC) {
+    article(where: { slug: $slug }) {
       title
       publishDate
       updatedAt
